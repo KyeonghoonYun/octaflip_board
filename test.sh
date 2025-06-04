fb46722f9f477aae733b7b3989f2699332834f5e
danielk <danielk5432@gmail.com>
2025-06-04 15:22:12 +0900

    error fix

--- test.sh
+++ test.sh
@@ -2,6 +2,6 @@ echo "start"
 # make clean
 # make all
 
-g++ -Iinclude ./lib/board.c -DDD ./lib/*.o -o main
+g++ -Iinclude ./lib/board.c -DDD ./lib/*.o -o
 
 #g++ -Iinclude ./lib/board.c client.c ./lib/*.o -o main
