d028e0d790a91e3e64a1862283576081f1787636
danielk <danielk5432@gmail.com>
2025-06-04 15:23:56 +0900

    flag error fixed

--- test.sh
+++ test.sh
@@ -2,6 +2,6 @@ echo "start"
 # make clean
 # make all
 
-g++ -Iinclude ./lib/board.c -DDD ./lib/*.o -o
+g++ -DDD -Iinclude ./lib/board.c ./lib/*.o -o main
 
 #g++ -Iinclude ./lib/board.c client.c ./lib/*.o -o main
